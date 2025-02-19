558ab057e093c6239c62b1ec4fe3e25d72c09327
Ahmar72 <syedahmerhusain@gmail.com>
2025-02-19 12:26:50 +0500

    add calc

--- calc_in_dart/main.dart
+++ calc_in_dart/main.dart
@@ -1,3 +1,44 @@
+import 'dart:io';
+
+num add(num a, num b) => a + b;
+num subtract(num a, num b) => a - b;
+num multiply(num a, num b) => a * b;
+num divide(num a, num b) {
+  if (b == 0) {
+    print('Error: Division by zero');
+    return double.nan;
+  }
+  return a / b;
+}
+
+num calculate(num a, num b, String operator) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      print('Invalid operator');
+      return double.nan;
+  }
+}
+
 void main() {
-	print("ahmar");
+  print('Enter the first number:');
+  num num1 = num.parse(stdin.readLineSync()!);
+
+  print('Enter the operator (+, -, *, /):');
+  String operator = stdin.readLineSync()!;
+
+  print('Enter the second number:');
+  num num2 = num.parse(stdin.readLineSync()!);
+
+  num result = calculate(num1, num2, operator);
+  if (!result.isNaN) {
+    print('Result: $result');
+  }
 }
